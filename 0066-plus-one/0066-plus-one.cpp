5d89ac831305d25683029732362ebc8ac06bfd60
Mohamed Faaris <74762980+Mohamed-faaris@users.noreply.github.com>
2025-04-02 00:55:47 +0530

    Time: 0 ms (100%), Space: 11.6 MB (34.93%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int carry = 1,i = digits.size() - 1;
+        while(carry&&i >= 0){
+            int sum = digits[i] + carry;
+            carry = sum/10;
+            digits[i--] = sum%10;
+        }
+        if(carry) digits.insert(digits.begin(), 1);
+
+        return digits;
+    }
+};
