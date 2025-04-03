809f2a5b1ae1374eb75721ea1a22669bff693cc6
Mohamed Faaris <74762980+Mohamed-faaris@users.noreply.github.com>
2025-04-04 02:04:54 +0530

    Time: 0 ms (100%), Space: 21.7 MB (80.31%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -12,8 +12,7 @@ public:
                     if(stk[stk.size() - 1] < 0)  break;
                     else if(stk[stk.size() - 1] < -i) stk.pop_back();
                     else if(stk[stk.size() - 1] > -i) {i =0;break;}
-                    else if(stk[stk.size() - 1] == -i) {i=0;stk.pop_back();break;}
-                    //else  {stk.push_back(i);break;}
+                    else {i=0;stk.pop_back();break;}
                 }
                 if(i) stk.push_back(i);
             }
