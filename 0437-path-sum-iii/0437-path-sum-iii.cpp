7032b628c3344876616d879df17a83fd51721740
Mohamed Faaris <74762980+Mohamed-faaris@users.noreply.github.com>
2025-04-03 12:29:32 +0530

    Time: 4 ms (82.16%), Space: 20.8 MB (44.99%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+template <typename K, typename V>
+std::ostream& operator<<(std::ostream& os, const std::unordered_map<K, V>& umap) {
+    for (const auto& pair : umap) {
+        os << pair.first << " -> " << pair.second << "\n";
+    }
+    return os;
+}
+class Solution {
+public:
+    unordered_map<long,int> fMap;
+    void dfs(int& count,TreeNode* root,long sum,int targetSum){
+        if(!root) return;
+        sum += root->val;
+        //cout<<root->val<<" "<<fMap<<"";
+        if(fMap. find(sum-targetSum) != fMap.end()) 
+            count += fMap[sum-targetSum];
+        fMap[sum] += 1;
+        dfs(count,root->left,sum,targetSum);
+        dfs(count,root->right,sum,targetSum);
+        fMap[sum] -= 1;
+    }
+    int pathSum(TreeNode* root, int targetSum) {
+        int count = 0;
+        fMap[0] = 1; 
+        dfs(count,root,0,targetSum);
+        return count;
+    }
+};
