52c81ba785ca67efe59e6e89ebca72b277cdb7e8
Mohamed Faaris <74762980+Mohamed-faaris@users.noreply.github.com>
2025-04-06 21:44:21 +0530

    Time: 47 ms (61.34%), Space: 55.2 MB (49.9%) - LeetHub

---
+++ 2732-counter/2732-counter.ts
@@ -0,0 +1,14 @@
+function createCounter(n: number): () => number {
+    let count = n - 1
+    return function() {
+        return count += 1
+    }
+}
+
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
