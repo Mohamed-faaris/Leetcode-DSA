b06bfe57b171cf6243c530e80aba1f383dc7b68a
Mohamed Faaris <74762980+Mohamed-faaris@users.noreply.github.com>
2025-08-23 14:40:46 +0530

    Time: 4 ms (63.77%), Space: 93.8 MB (90.89%) - LeetHub

---
+++ 3189-find-champion-ii/3189-find-champion-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findChampion(int n, vector<vector<int>>& edges) {
+        vector<int> in(n,0);
+        for(vector<int>& edge:edges){
+            in[edge[1]]+=1;
+        }
+        int champion = -1;
+        for(int i = 0;i < n;i++){
+            if(in[i] == 0){
+                if(champion != -1) return -1;
+                champion = i;
+            }
+        }
+        return champion;
+    }
+};
